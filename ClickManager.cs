45938b436dab7dce6f0b07018c0a5b540df9feaa
Hackbright Student <student@hackbrightacademy.com>
2021-06-27 21:46:36 -0700

    Updates functionality for user to purchase autoclickers.

--- ClickManager.cs
+++ ClickManager.cs
@@ -10,14 +10,23 @@ public class ClickManager : MonoBehaviour
     void Update()
     {
         // Loop until reach end of the list.
-        for(int i = 0; i < autoClickersLastTime; i ++)
+        for(int i = 0; i < autoClickersLastTime.Count; i ++)
         {
-            // Check sto see if value is more than one second ago.
+            // Checks to see if value is more than one second ago.
             if(Time.time - autoClickersLastTime[i] >= 1.0f)
             {
                 autoClickersLastTime[i] = Time.time;
                 EnemyManager.instance.curEnemy.Damage();
             }
+        }
+    }
+
+    public void OnBuyAutoClicker()
+    {
+        if(GameManager.instance.gold >= autoClickerPrice)
+        {
+            GameManager.instance.TakeGold(autoClickerPrice);
+            autoClickersLastTime.Add(Time.time);
         }
     }
 }
