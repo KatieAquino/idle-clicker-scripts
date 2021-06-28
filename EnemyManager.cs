500f65519fe791a973179843e9f6ea9f772b8ed2
Hackbright Student <student@hackbrightacademy.com>
2021-06-27 21:05:40 -0700

    Begins functionality for grabbing random enemy for clicker.

--- EnemyManager.cs
+++ EnemyManager.cs
@@ -6,4 +6,20 @@ public class EnemyManager : MonoBehaviour
 {
     // Array of prefabs (GameObject)
     public GameObject[] enemyPrefabs;
+    public Enemy curEnemy;
+    public static EnemyManager instance;
+    public Transform canvas;
+
+    void Awake()
+    {
+        instance = this;
+    }
+
+    void CreateNewEnemy()
+    {
+        GameObject enemyToSpawn = enemyPrefabs[Random.Range(0, enemyPrefabs.Length)];
+        GameObject obj = Instantiate(enemyToSpawn, canvas);
+
+        curEnemy = obj.GetComponent<Enemy>();
+    }
 }
