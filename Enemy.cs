58c1cf67aa0b5c9fd333261c4cb4212cf4a83d94
Hackbright Student <student@hackbrightacademy.com>
2021-06-27 20:53:43 -0700

    Initial Commit

---
+++ Enemy.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Enemy : MonoBehaviour
+{
+    public Image healthBarFill;
+    public int curHp;
+    public int maxHp;
+    public int goldToGive;
+
+    public void Damage()
+    {
+        // Subtract 1 from curHP
+        curHp--;
+        // Update health bar
+        healthBarFill.fillAmount = (float)curHp / (float)maxHp;
+
+
+        if(curHp <= 0)
+        {
+            Defeated();
+        }
+
+    }
+
+    public void Defeated()
+    {
+        Debug.Log("Defeated");
+    }
+}
