fcf3f74d7b00212f19eb4f0d263a66eb6e152566
Hackbright Student <student@hackbrightacademy.com>
2021-06-27 21:51:11 -0700

    Creates updating text for number of autoclickers purchased.

--- ClickManager.cs
+++ ClickManager.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class ClickManager : MonoBehaviour
 {
     public List<float> autoClickersLastTime = new List<float>();
     public int autoClickerPrice;
+    public TextMeshProUGUI quantityText;
 
     void Update()
     {
@@ -27,6 +29,8 @@ public class ClickManager : MonoBehaviour
         {
             GameManager.instance.TakeGold(autoClickerPrice);
             autoClickersLastTime.Add(Time.time);
+
+            quantityText.text = "x: " + autoClickersLastTime.Count.ToString();
         }
     }
 }
